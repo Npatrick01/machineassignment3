9551c201a631343b416bd164f192e0b80d1d7fb1
npatos <npatos01@gmail.com>
2020-12-03 01:34:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,21 @@ Created on Sun Nov 29 00:43:41 2020
 """
 import streamlit as st 
 import tensorflow as tf
-import keras
 import joblib,os
 import numpy as np
 import pandas as pd
 from keras.models import load_model
 from keras.preprocessing.text import one_hot
 from keras.preprocessing.sequence import pad_sequences
+from nltk.tokenize import word_tokenize
+from keras.preprocessing.text import Tokenizer
+import re
+from nltk.corpus import stopwords
+
 
 model=tf.keras.models.load_model( 'model.h5' )
 
-st.title("News Classifier")
+st.title("Email Classifier")
 	# st.subheader("ML App with Streamlit")
 html_temp = """
 	<div style="background-color:blue;padding:10px">
@@ -26,20 +30,27 @@ html_temp = """
 	"""
 st.markdown(html_temp,unsafe_allow_html=True)
     
-news_text = st.text_area("Enter News Here","Type Here")
+news_text = st.text_area("Enter email Here","Type Here")
+def clean_text(text):
+        text = re.sub('[^a-zA-Z]', ' ', text)  
+        text = text.lower()  
+        text = text.split(' ')  
+        text = [w for w in text if not w in set(stopwords.words('english'))] 
+        text = ' '.join(text)    
+            
+        return text
 if st.button("Classify"):
-    vocab_size = 50
-    encoded_docs = [one_hot(d, vocab_size) for d in news_text]
-    max_length = 10
-    padded_docs = pad_sequences(encoded_docs, maxlen=max_length, padding= 'post' )
-    y_pred = model.predict(padded_docs)
-    np.set_printoptions(formatter={'float': '{: 0.2f}'.format})
-    y_pred*100
-
-    
+    tokenizer_obj=Tokenizer()
+    tokenizer_obj.fit_on_texts(news_text)
+    sequences=tokenizer_obj.texts_to_sequences(news_text)
+    text_cleaned = clean_text(news_text )
+    test_sequences = tokenizer_obj.texts_to_sequences(text_cleaned)
+    test_data = pad_sequences(test_sequences, maxlen=100)
+    predictions= model.predict_classes(test_data)
+   
     # load the model
-    st.write(np.argmax(y_pred[0]))  
-    if(y_pred[0]==1):
+    st.write(np.argmax( predictions[0]))  
+    if( predictions[0]==1):
         st.write('sparm')
     else:
         st.write('Ham')
